51e9fbb51b55fdb5762f6b9f32dc5143f5bcd6e3
小顶 <2234050725@qq.com>
2022-02-16 07:12:51 -0800

    Second Initial

--- tools/urls.py
+++ tools/urls.py
@@ -1,4 +1,8 @@
+
 from django.urls import path
 
+from tools.views import project
+
 urlpatterns = [
+    path('project/list', project.project_list, name='project_list')
 ]
---
+++ tools/views/project.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def project_list(request):
+
+    return render(request, 'project_list.html')
